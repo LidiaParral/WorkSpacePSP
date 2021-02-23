f751f784e79c38d4278f3f9a80edb0411fc9d346
LidiaParral <72860314+LidiaParral@users.noreply.github.com>
2021-02-23 02:53:23 +0100

    Update Exercise
    -Update 02_EjercicioVideojuegoCRUD

--- 02_EjercicioVideojuegoCRUD/src/main/java/com/ejercicioVideojuego/controlador/ServicioMensaje.java
+++ 02_EjercicioVideojuegoCRUD/src/main/java/com/ejercicioVideojuego/controlador/ServicioMensaje.java
@@ -1,10 +1,35 @@
 package com.ejercicioVideojuego.controlador;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ejercicioVideojuego.modelo.entidad.Videojuego;
+
 
 @RestController
 public class ServicioMensaje {
 	
+	@Autowired
+	private Videojuego v1;
 	
+	@GetMapping("videojuego")
+	public Videojuego obtenerVideojuego() {
+		
+		v1.setNombre("Fortnite");
+		v1.setCompania("Epic Games");
+		v1.setPrecio(22.70);
+		
+		return v1;
+	}
+	
+	
+	//*Método MODIFICAR
+	@PutMapping("videojuego")
+	public Videojuego modificar() {
+		v1.setPrecio(v1.getPrecio() + 10);
+		return v1;
+	}
 
 }
--- 02_EjercicioVideojuegoCRUD/src/main/java/com/ejercicioVideojuego/modelo/entidad/Videojuego.java
+++ 02_EjercicioVideojuegoCRUD/src/main/java/com/ejercicioVideojuego/modelo/entidad/Videojuego.java
@@ -8,7 +8,39 @@ import org.springframework.stereotype.Component;
 public class Videojuego {
 	
 	private int id;
-	private 
+	private String nombre;
+	private String compania;
+	private double precio;
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCompania() {
+		return compania;
+	}
+	public void setCompania(String compania) {
+		this.compania = compania;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	
+	
+	
 	
 
 }
